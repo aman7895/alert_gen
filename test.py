d5d31cdae79ccb9699e97e6ee86959f515b03e01
aman7895 <aman.middha@gmail.com>
2021-01-14 22:03:31 +0800

    Testing singleton feature and added business logic to test message feature

--- test.py
+++ test.py
@@ -9,3 +9,12 @@ def test_one_message_multiple_calls():
         x += 1 if alerts.send_alert("This should print twice") else 0
         time.sleep(1)
     assert x == 2
+
+
+def test_cannot_create_instance_of_alert_manager():
+    is_error_thrown = False
+    try:
+        AlertManager()
+    except:
+        is_error_thrown = True
+    assert is_error_thrown
